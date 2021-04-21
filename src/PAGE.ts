13a3b35e3293a19b5cad80c4bad1a988c8800146
Evans Owino <9787512+eowino@users.noreply.github.com>
2021-04-21 16:01:21 +0100

    refactor: trim page

--- src/PAGE.ts
+++ src/PAGE.ts
@@ -1,12 +1,1 @@
-export const PAGE = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <script src="https://imasdk.googleapis.com/js/sdkloader/ima3_dai.js"></script>
-    </head>
-    </html>
-`;
+export const PAGE = `<script src="https://imasdk.googleapis.com/js/sdkloader/ima3_dai.js"></script>`;
